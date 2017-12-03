0e511bb614fddc7b4b84e9fee978602beaf9f2a3
Azir Yasin <aziryasin@gmail.com>
2017-12-03 10:48:00 +0530

    DB Update
    please add mysql uname and pwd

--- db.php
+++ db.php
@@ -3,68 +3,78 @@
 
 	$query="create database kamu";
 	if(mysqli_query($con,$query)){
-		echo "Success database creation";
+		echo "Success database creation<br/>";
 	}
 	else{
-		echo "failed <br>".mysqli_error($con);
+		echo "failed <br/>".mysqli_error($con);
 	}
 	mysqli_select_db($con,"kamu");
 
-	$query="create table user(eid int UNIQUE PRIMARY KEY NOT NULL AUTO_INCREMENT,username varchar(20) UNIQUE,password varchar(32),role varchar(20))";
+	$query="create table user(eid int UNIQUE PRIMARY KEY NOT NULL AUTO_INCREMENT,name varchar(50),username varchar(20) UNIQUE,password varchar(32),role varchar(20))";
 	if(mysqli_query($con,$query)){
 		echo "Success user<br/>";
 	}
 	else{
-		echo "failed <br>".mysqli_error($con);
+		echo "failed <br/>".mysqli_error($con);
 	}
 
 
-	$query="create table orderInfo(orderId int UNIQUE PRIMARY KEY NOT NULL AUTO_INCREMENT,username varchar(20),orderDate DATE,foodName varchar(20),meal varchar(20),price FLOAT(8,2),qty int,total FLOAT(8,2))";
+	$query="create table orderInfo(orderId int UNIQUE PRIMARY KEY NOT NULL AUTO_INCREMENT,eid int,orderDate DATE,foodName varchar(20),meal varchar(20),price FLOAT(8,2),qty int,total FLOAT(8,2))";
 	if(mysqli_query($con,$query)){
-		echo "Success orderInfo";
+		echo "Success orderInfo<br/>";
 	}
 	else{
-		echo "failed <br>".mysqli_error($con);
+		echo "failed <br/>".mysqli_error($con);
 	}
 
 
-	$query="create table food(foodId int UNIQUE PRIMARY KEY,foodName varchar(20),type varchar(20),price FLOAT(8,2))";
+	$query="create table food(foodId int UNIQUE PRIMARY KEY AUTO_INCREMENT,foodName varchar(20) UNIQUE,type varchar(20),price FLOAT(8,2))";
 	if(mysqli_query($con,$query)){
-		echo "Success food";
+		echo "Success food<br/>";
 	}
 	else{
-		echo "failed <br>".mysqli_error($con);
+		echo "failed <br/>".mysqli_error($con);
 	}
 
 	$query="create table audit(auditId int UNIQUE PRIMARY KEY NOT NULL AUTO_INCREMENT,username varchar(20),totalDue FLOAT(8,2))";
 	if(mysqli_query($con,$query)){
-		echo "Success audit";
+		echo "Success audit<br/>";
 	}
 	else{
-		echo "failed <br>".mysqli_error($con);
+		echo "failed <br/>".mysqli_error($con);
 	}
 
-	$query="insert into user(username,password,role) values('admin@kamu.com','0192023a7bbd73250516f069df18b500','admin')";
+	$query="insert into user(name,username,password,role) values('Administrator','admin@kamu.com','0192023a7bbd73250516f069df18b500','admin')";
 	if(mysqli_query($con,$query)){
-		echo "Success admin insert";
+		echo "Success admin insert<br/>";
 	}
 	else{
-		echo "failed <br>".mysqli_error($con);
+		echo "failed <br/>".mysqli_error($con);
 	}
 
-	$query="insert into user(username,password,role) values('seller@kamu.com','1e4970ada8c054474cda889490de3421','seller')";
+	$query="insert into user(name,username,password,role) values('The Seller','seller@kamu.com','1e4970ada8c054474cda889490de3421','seller')";
 	if(mysqli_query($con,$query)){
-		echo "Success seller insert";
+		echo "Success seller insert<br/>";
 	}
 	else{
-		echo "failed <br>".mysqli_error($con);
+		echo "failed <br/>".mysqli_error($con);
 	}
 
-	$query="insert into user(username,password,role) values('buyer@kamu.com','40e868c2d8064888a2a3365a63a84d58','buyer')";
+	$query="insert into user(name,username,password,role) values('The Buyer','buyer@kamu.com','40e868c2d8064888a2a3365a63a84d58','buyer')";
 	if(mysqli_query($con,$query)){
-		echo "Success Buyer insert";
+		echo "Success Buyer insert<br/>";
 	}
 	else{
-		echo "failed <br>".mysqli_error($con);
+		echo "failed <br/>".mysqli_error($con);
 	}
+
+	$query="insert into food(foodName,type,price) values('Rice','Non-Veg',100.00)";
+	if(mysqli_query($con,$query)){
+		echo "Success Food Rice insert<br/>";
+	}
+	else{
+		echo "failed <br/>".mysqli_error($con);
+	}
+
+
 ?>
